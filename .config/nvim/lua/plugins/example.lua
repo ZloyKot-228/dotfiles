558b7443db49cd750666579fcdd220d575760d93
ZK <zloyk0t@proton.me>
2026-03-12 21:59:16 +0200

    Add auto save

--- .config/nvim/lua/plugins/example.lua
+++ .config/nvim/lua/plugins/example.lua
@@ -53,4 +53,20 @@ return {
       },
     },
   },
+
+  {
+    "Pocco81/auto-save.nvim",
+    opts = {
+      execution_message = {
+        message = function()
+          return ""
+        end,
+      },
+    },
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+       opts= {autoformat = false},
+  },
 }
